75b655d3a9acc0665c11afd35540d9c5ffea1b08
TheMazzock <44572191+TheMazzock@users.noreply.github.com>
2018-11-07 13:28:16 +0100

    Update dbzmoney.py

--- dbzmoney.py
+++ dbzmoney.py
@@ -1,6 +1,7 @@
 from twx.botapi import TelegramBot, ReplyKeyboardMarkup
 
 bot = TelegramBot('783761569:AAFk4MtxJkw0PmIsQf1uUfMYCo0wGLfsFSA')
+print(bot)
 bot.update_bot_info().wait()
 print(bot.username)
 
