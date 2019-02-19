14ec597cf53371064482378a8c7b8b229234192f
TheMazzock <44572191+TheMazzock@users.noreply.github.com>
2019-02-19 11:46:38 +0100

    Update app.py

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ def get_service(service_name='sheets', api_version='v4'):
 
 service = get_service()
 spreadsheet_id = os.environ["GOOGLE_SPREADSHEET_ID"]
-range_name = os.environ["database!B1:B100"]
+range_name = "database!B1:B100"
 result = service.spreadsheets().values().get(spreadsheetId=spreadsheet_id, range=range_name).execute()
 values = result.get('values', [])
 print(values)
