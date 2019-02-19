881e6b3b1bf0c5d2f46d791085f815e5e84876fb
TheMazzock <44572191+TheMazzock@users.noreply.github.com>
2019-02-19 12:29:21 +0100

    Update app.py

--- app.py
+++ app.py
@@ -39,6 +39,23 @@ print(values)
 start_keyboard = [["Situazione","Conti del mese"],["Inserisci"],["Aiuto"]]
 start_markup = ReplyKeyboardMarkup(keyboard=start_keyboard, one_time_keyboard=False)
 
+def on_chat_message(msg):
+    content_type, chat_type, chat_id = telepot.glance(msg)
+
+    keyboard = InlineKeyboardMarkup(inline_keyboard=[
+                   [InlineKeyboardButton(text='Press me', callback_data='press')],
+               ])
+
+    bot.sendMessage(chat_id, 'Use inline keyboard', reply_markup=keyboard)
+
+def on_callback_query(msg):
+    query_id, from_id, query_data = telepot.glance(msg, flavor='callback_query')
+    print('Callback Query:', query_id, from_id, query_data)
+
+    bot.answerCallbackQuery(query_id, text='Got it')
+
+
+"""
 def handle(msg):
     content_type, chat_type, chat_id = telepot.glance(msg)
     print(content_type, chat_type, chat_id)
@@ -59,7 +76,7 @@ def handle(msg):
         bot.sendMessage(chat_id,'Bot sviluppato da Alessio')
     else:
         bot.sendMessage(chat_id,text)
-
+"""
 
 """
 app = Flask(__name__)
@@ -83,7 +100,8 @@ if __name__ == '__main__':
 
 TOKEN = os.environ["BOT_TOKEN"]
 bot = telepot.Bot(TOKEN)
-MessageLoop(bot, handle).run_as_thread()
+MessageLoop(bot, {'chat': on_chat_message,
+                  'callback_query': on_callback_query}).run_as_thread()
 print('Listening ....')
 # Keep the program running.
 while 1:
