0e11b6f2ca045707aefbd6d3a7a32391b3fee348
TheMazzock <44572191+TheMazzock@users.noreply.github.com>
2019-05-24 12:15:23 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import os
+'''import os
 from flask import Flask, render_template
 import googleapiclient.discovery
 from google.oauth2 import service_account
@@ -142,3 +142,29 @@ print('Listening ....')
 # Keep the program running.
 while 1:
     time.sleep(10)
+'''
+
+from telegram.ext import Updater
+TOKEN = os.environ["BOT_TOKEN"]
+updater = Updater(token='TOKEN', use_context=True)
+dispatcher = updater.dispatcher
+
+import logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                     level=logging.INFO)
+
+def start(update, context):
+    context.bot.send_message(chat_id=update.message.chat_id, text="I'm a bot, please talk to me!")
+    
+from telegram.ext import CommandHandler
+start_handler = CommandHandler('start', start)
+dispatcher.add_handler(start_handler)
+
+def echo(update, context):
+    context.bot.send_message(chat_id=update.message.chat_id, text=update.message.text)
+
+from telegram.ext import MessageHandler, Filters
+echo_handler = MessageHandler(Filters.text, echo)
+dispatcher.add_handler(echo_handler)
+    
+updater.start_polling()
