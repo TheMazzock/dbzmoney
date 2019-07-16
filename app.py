6d14b11517c55d881dc22a05252daaa4f470eede
TheMazzock <44572191+TheMazzock@users.noreply.github.com>
2019-07-16 11:17:50 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask, render_template
 import googleapiclient.discovery
 from google.oauth2 import service_account
+'''
 import telepot, time, sqlite3, random, csv
 from telepot.loop import MessageLoop
 from telepot.namedtuple import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
@@ -119,6 +120,16 @@ def on_callback_query(msg):
         ritorna(query_id,from_id)
         
 
+TOKEN = os.environ["BOT_TOKEN"]
+bot = telepot.Bot(TOKEN)
+MessageLoop(bot, {'chat': on_chat_message,
+                  'callback_query': on_callback_query}).run_as_thread()
+print('Listening ....')
+# Keep the program running.
+while 1:
+    time.sleep(10)
+
+
 
 """
 def handle(msg):
@@ -142,8 +153,8 @@ def handle(msg):
     else:
         bot.sendMessage(chat_id,text)
 """
+'''
 
-"""
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
@@ -161,13 +172,5 @@ def homepage():
 
 if __name__ == '__main__':
     app.run(debug=True)
-"""
 
-TOKEN = os.environ["BOT_TOKEN"]
-bot = telepot.Bot(TOKEN)
-MessageLoop(bot, {'chat': on_chat_message,
-                  'callback_query': on_callback_query}).run_as_thread()
-print('Listening ....')
-# Keep the program running.
-while 1:
-    time.sleep(10)
+
