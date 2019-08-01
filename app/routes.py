79bf84ba913dcc7f13e829b38c22eb469786a752
TheMazzock <44572191+TheMazzock@users.noreply.github.com>
2019-08-01 16:11:32 +0200

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from app import app, get_service
 import os
-from flask import render_template
+from flask import render_template, flash, redirect
 from app.forms import LoginForm
 
 @app.route('/', methods=['GET'])
@@ -15,7 +15,11 @@ def homepage():
 
     return render_template('index.html', values=values)
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/index')
     return render_template('login.html', title='Sign In', form=form)
