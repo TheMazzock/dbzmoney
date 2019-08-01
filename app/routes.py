816bc37487072abff93142859aa66bfe959a5e61
TheMazzock <44572191+TheMazzock@users.noreply.github.com>
2019-08-01 10:10:11 +0200

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,13 @@
 from app import app
 
-@app.route('/')
-@app.route('/index')
-def index():
-    return "Hello, World!"
+@app.route('/', methods=['GET'])
+def homepage():
+    service = get_service()
+    spreadsheet_id = os.environ["GOOGLE_SPREADSHEET_ID"]
+    range_name = os.environ["GOOGLE_CELL_RANGE"]
+
+    result = service.spreadsheets().values().get(
+        spreadsheetId=spreadsheet_id, range=range_name).execute()
+    values = result.get('values', [])
+
+    return render_template('index.html', values=values)
