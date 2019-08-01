e8a28d8f3d8a2b5d6afe6873ba0f621599f592f1
TheMazzock <44572191+TheMazzock@users.noreply.github.com>
2019-08-01 16:19:32 +0200

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -4,6 +4,7 @@ from flask import render_template, flash, redirect, url_for
 from app.forms import LoginForm
 
 @app.route('/', methods=['GET'])
+@app.route('/index', methods=['GET'])
 def homepage():
     service = get_service()
     spreadsheet_id = os.environ["GOOGLE_SPREADSHEET_ID"]
