bbc1f7076961deeddf4bc784aeff1c121952ddaa
TheMazzock <44572191+TheMazzock@users.noreply.github.com>
2019-08-01 09:57:43 +0200

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,44 @@
-from flask import Flask
+import os
+from flask import Flask, render_template
+import googleapiclient.discovery
+from google.oauth2 import service_account
+
+def get_credentials():
+  scopes = ["https://www.googleapis.com/auth/spreadsheets.readonly"]
+  GOOGLE_PRIVATE_KEY = os.environ["GOOGLE_PRIVATE_KEY"]
+  # The environment variable has escaped newlines, so remove the extra backslash
+  GOOGLE_PRIVATE_KEY = GOOGLE_PRIVATE_KEY.replace('\\n', '\n')
+  account_info = {
+      "private_key": GOOGLE_PRIVATE_KEY,
+      "client_email": os.environ["GOOGLE_CLIENT_EMAIL"],
+      "token_uri": "https://accounts.google.com/o/oauth2/token",
+    }
+  credentials = service_account.Credentials.from_service_account_info(account_info, scopes=scopes)
+  return credentials
+
+def get_service(service_name='sheets', api_version='v4'):
+  credentials = get_credentials()
+  service = googleapiclient.discovery.build(service_name, api_version, credentials=credentials)
+  return service
+
+service = get_service()
+spreadsheet_id = os.environ["GOOGLE_SPREADSHEET_ID"]
+
+range_name = "database!B1:B100"
+result = service.spreadsheets().values().get(spreadsheetId=spreadsheet_id, range=range_name).execute()
+values = result.get('values', [])
+  
+
+
+
+
+
+
+
+
 app = Flask(__name__)
 from app import routes
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
