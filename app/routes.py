e91eeff834002e52d6ac2ce7a19956c194a76c37
TheMazzock <44572191+TheMazzock@users.noreply.github.com>
2019-08-05 12:55:56 +0200

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -2,16 +2,22 @@ from app import app, get_service
 import os
 from flask import render_template, flash, redirect, url_for
 from app.forms import LoginForm
+from config import Config
 
 @app.route('/', methods=['GET'])
 @app.route('/index', methods=['GET'])
 def index():
+    """
     service = get_service()
     spreadsheet_id = os.environ["GOOGLE_SPREADSHEET_ID"]
     range_name = os.environ["GOOGLE_CELL_RANGE"]
 
     result = service.spreadsheets().values().get(
         spreadsheetId=spreadsheet_id, range=range_name).execute()
+    values = result.get('values', [])
+    """
+    result = Config.SERVICE.spreadsheets().values().get(
+        spreadsheetId=Config.SHEET, range=Config.RANGE_SITUAZIONE).execute()
     values = result.get('values', [])
 
     return render_template('index.html', values=values)
